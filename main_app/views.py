0c878decc3dc39bdf8cf261a1b30fa314dd48501
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-31 15:55:03 -0400

    create dog view template and url

--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,20 @@ def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 def about(request):
   return render(request, 'about.html')
+
+class Dog:
+  def __init__(self, name, breed, description, age): 
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Lolo', 'tabby', 'Kinda rude.', 3),
+  Dog('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Dog('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Dog('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+def dog_index(request):
+  return render(request, 'dogs/index.html', {'dogs': dogs})
