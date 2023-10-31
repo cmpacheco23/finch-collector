aefa6ebed36f57735be0be0baaf5e2ddcd100563
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-31 16:42:28 -0400

    add update functionality and delete UI

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,4 +12,7 @@ class Dog(models.Model):
 
   def __str__(self):
       return self.name
+  
+  def get_absolute_url(self):
+      return reverse("dog-detail", kwargs={"dog_id": self.id})
   
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Dog
 # Add the following import
@@ -21,3 +21,11 @@ def dog_detail(request, dog_id):
 class DogCreate(CreateView):
   model = Dog
   fields = ['name', 'breed', 'description', 'pawrent', 'age']
+  
+class DogUpdate(UpdateView):
+  model = Dog
+  fields = ['breed', 'description', 'pawrent', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url= '/dogs/'
