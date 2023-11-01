d52f1052e1a99d6e3aa7c725f73cc616d3805571
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-11-01 11:48:15 -0400

    update dog model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
 TIMING = (
@@ -25,6 +26,7 @@ class Dog(models.Model):
   description = models.TextField(max_length=250)
   age = models.IntegerField()
   bowls = models.ManyToManyField(Bowl)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
       return self.name
