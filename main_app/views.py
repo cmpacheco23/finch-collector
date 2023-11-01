73bd2f711a9e479f83291226af0a5bf4dece7efe
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-11-01 12:22:26 -0400

    implement auth for view functions and classes

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,8 @@ from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Dog, Bowl
 from .forms import WalkForm
 # Add the following import
@@ -15,16 +17,19 @@ class Home(LoginView):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def dog_index(request):
   dogs = Dog.objects.filter(user=request.user)
   return render(request, 'dogs/index.html', {'dogs': dogs})
 
+@login_required
 def dog_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
   bowls_dog_doesnt_have = Bowl.objects.exclude(id__in=dog.bowls.all().values_list('id'))
   walk_form = WalkForm()
   return render(request, 'dogs/detail.html', {'dog': dog, 'walk_form': walk_form, 'bowls': bowls_dog_doesnt_have})
 
+@login_required
 def add_walk(request, dog_id):
   form = WalkForm(request.POST)
   if form.is_valid():
@@ -33,11 +38,11 @@ def add_walk(request, dog_id):
     new_walk.save()
   return redirect('dog-detail', dog_id=dog_id)
 
-class DogCreate(CreateView):
+class DogCreate(LoginRequiredMixin, CreateView):
   model = Dog
   fields = ['name', 'breed', 'description', 'age']
   
-class DogUpdate(UpdateView):
+class DogUpdate(LoginRequiredMixin, UpdateView):
   model = Dog
   fields = ['breed', 'description', 'age']
 
@@ -46,28 +51,29 @@ class DogUpdate(UpdateView):
     return super().form_valid(form)
   
 
-class DogDelete(DeleteView):
+class DogDelete(LoginRequiredMixin, DeleteView):
   model = Dog
   success_url= '/dogs/'
 
-class BowlCreate(CreateView):
+class BowlCreate(LoginRequiredMixin, CreateView):
   model = Bowl
   fields = '__all__'
 
-class BowlList(ListView):
+class BowlList(LoginRequiredMixin, ListView):
   model = Bowl
 
-class BowlDetail(DetailView):
+class BowlDetail(LoginRequiredMixin, DetailView):
   model = Bowl
 
-class BowlUpdate(UpdateView):
+class BowlUpdate(LoginRequiredMixin, UpdateView):
   model = Bowl
   fields = ['size', 'color']
 
-class BowlDelete(DeleteView):
+class BowlDelete(LoginRequiredMixin, DeleteView):
   model = Bowl
   success_url = '/bowls/'
 
+@login_required
 def assoc_bowl(request, dog_id, bowl_id):
   Dog.objects.get(id=dog_id).bowls.add(bowl_id)
   return redirect('dog-detail', dog_id=dog_id)
